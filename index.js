bc2b06190ffb594e59ed25ed599e4e0acd25cb43
rfalaize <rhome.falaize@gmail.com>
2019-11-03 10:33:30 +0800

    add auth

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
+const env = require("dotenv").config();
 
 // create express app
 const app = express();
+app.use(cors());
+app.use(bodyParser.json({ limit: "10mb" }));
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,7 +17,6 @@ app.use(bodyParser.json());
 // **************************************************
 // configuring the database
 // **************************************************
-const dbCreds = require("./config/mongo.js");
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 // load models
@@ -21,7 +24,15 @@ const HealthCheck = require("./model/healthCheck.js");
 const VoiceSample = require("./model/voiceSample.js");
 
 console.log("Connecting to mongodb...");
-const uri = "mongodb+srv://" + dbCreds.mongoUser + ":" + dbCreds.mongoPassword + "@" + dbCreds.url + "/voice-app?retryWrites=true&w=majority";
+const uri =
+    "mongodb+srv://" +
+    process.env.MONGODB_USER +
+    ":" +
+    process.env.MONGODB_PWD +
+    "@" +
+    process.env.MONGODB_URL +
+    "/voice-app?retryWrites=true&w=majority";
+
 mongoose
     .connect(uri, {
         useNewUrlParser: true,
@@ -46,6 +57,10 @@ app.get("/", (req, res) => {
 });
 
 app.get("/health-check", (req, res) => {
+    if (!validateAdminRequest(req)) {
+        res.sendStatus(403);
+        return;
+    }
     console.log("Get health check...");
     // query
     HealthCheck.find({})
@@ -69,6 +84,10 @@ app.get("/health-check", (req, res) => {
 });
 
 app.get("/voice-samples", (req, res) => {
+    if (!validateAdminRequest(req)) {
+        res.sendStatus(403);
+        return;
+    }
     console.log("Fetching samples...");
     // query
     VoiceSample.find({})
@@ -92,21 +111,26 @@ app.get("/voice-samples", (req, res) => {
 });
 
 app.post("/voice-sample/save", (req, res) => {
-    const sample = new VoiceSample({
-        ip: req.headers["x-forwarded-for"] || req.connection.remoteAddress,
-        audio: req.body.audio
-    });
+    if (!validateUserRequest(req)) {
+        res.sendStatus(403);
+        return;
+    }
+    const sample = new VoiceSample(req.body);
     sample.save(function(mongoError) {
         if (mongoError) {
             console.log("An error occured while saving sample:", mongoError);
-            res.json({ status: "KO", errorMessage: mongoError.message });
+            res.json({ status: false, errorMessage: mongoError.message });
         }
-        console.log("Success! document saved:", sample);
-        res.json({ status: "OK", id: sample._id });
+        console.log("Success! document saved:", sample._id);
+        res.json({ status: true, id: sample._id });
     });
 });
 
 app.delete("/voice-sample/delete/:id", (req, res) => {
+    if (!validateUserRequest(req)) {
+        res.sendStatus(403);
+        return;
+    }
     VoiceSample.findByIdAndRemove(req.params.id, function(mongoError) {
         if (mongoError) handleError(mongoError, res);
         const message = "Document " + req.params.id + " deleted successfully.";
@@ -122,6 +146,20 @@ function handleError(err, res) {
         success: false,
         errorMessage: error
     });
+}
+
+// **************************************************
+// validate routes
+// **************************************************
+function validateAdminRequest(req) {
+    if (req.headers["node_api_secret"] === process.env.NODE_API_SECRET_ADMIN) return true;
+    return false;
+}
+
+function validateUserRequest(req) {
+    if (req.headers["node_api_secret"] === process.env.NODE_API_SECRET_ADMIN) return true;
+    if (req.headers["node_api_secret"] === process.env.NODE_API_SECRET_USER) return true;
+    return false;
 }
 
 // **************************************************
--- model/voiceSample.js
+++ model/voiceSample.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
 const collectionName = "voice-samples";
 // note: ** Mongoose automatically looks for the plural, lowercased version of the collection name. **
-module.exports = mongoose.model(collectionName, new mongoose.Schema({ ip: String, audio: [[Number]] }));
+module.exports = mongoose.model(
+    collectionName,
+    new mongoose.Schema({ name: String, email: String, question: String, frequenciesData: [], timeDomainData: [] })
+);
